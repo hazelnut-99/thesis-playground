0b0c6646484cf35c7d9434ddca5883c92e921291
hongshu <hongshuyan99@outlook.com>
2025-02-23 05:35:20 -0500

    analysis script

--- python/replay-trace-to-cachelib/generate_report.sh
+++ python/replay-trace-to-cachelib/generate_report.sh
@@ -4,7 +4,7 @@
 output_csv="outcome/report.csv"
 
 # Write the header to the CSV file
-echo "cacheSizeMB,rebalanceStrategy,allocator,numCacheGet,numCacheGetMisses" > $output_csv
+echo "cacheSizeMB,rebalanceStrategy,allocator,numCacheGet,numCacheGetMisses,allocSuccessRate,rebalanceNumRebalancedSlabs,getPerSec,setPerSec" > $output_csv
 
 # Iterate over each subdirectory in the outcome directory
 for dir in outcome/*/; do
@@ -18,9 +18,13 @@ for dir in outcome/*/; do
         # Extract values from std.out
         numCacheGet=$(grep -m 1 "Num Cache Gets  :" "$dir/std.out" | awk -F': ' '{print $2}' | tr -d ',')
         numCacheGetMisses=$(grep -m 1 "Num Cache Gets Misses  :" "$dir/std.out" | awk -F': ' '{print $2}' | tr -d ',')
+        allocSuccessRate=$(grep -m 1 "Alloc Attempts" "$dir/std.out" | awk -F'Success: ' '{print $2}' | tr -d '%' | tr -d ' ')
+        rebalanceNumRebalancedSlabs=$(grep -m 1 "Rebalance Num Rebalanced Slabs" "$dir/std.out" | awk -F': ' '{print $2}' | tr -d ' ')
+        getPerSec=$(grep -m 1 "get       :" "$dir/std.out" | awk -F': ' '{print $2}' | awk '{print $1}' | tr -d ',' | tr -d '/s')
+        setPerSec=$(grep -m 1 "set       :" "$dir/std.out" | awk -F': ' '{print $2}' | awk '{print $1}' | tr -d ',' | tr -d '/s')
 
         # Write the values to the CSV file
-        echo "$cacheSizeMB,$rebalanceStrategy,$allocator,$numCacheGet,$numCacheGetMisses" >> $output_csv
+        echo "$cacheSizeMB,$rebalanceStrategy,$allocator,$numCacheGet,$numCacheGetMisses,$allocSuccessRate,$rebalanceNumRebalancedSlabs,$getPerSec,$setPerSec" >> $output_csv
     fi
 done
 
